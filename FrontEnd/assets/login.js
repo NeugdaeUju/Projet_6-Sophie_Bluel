d7faed31fb6c60a583d2792aea5ecb3bfe0ae565
Leïla Pelletier <leila.plltr@gmail.com>
2025-07-23 11:34:05 +0200

    Ecouteur sur le bouton d'envoie du formulaire de connexion

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -2,3 +2,13 @@
 const form = document.querySelector("form");
 const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
+
+// Ecouteur sur l'envoie du formulaire de connexion
+form.addEventListener("submit", (event)=> {
+    event.preventDefault();
+    if (emailInput.value.trim() === "" || passwordInput.value.trim() === ""){
+        alert("Tous les champs sont requis.");
+        return;
+    }
+})
+
