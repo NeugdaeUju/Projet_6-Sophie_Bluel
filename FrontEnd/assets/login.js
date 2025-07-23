4af43c467dd62467cc2e7e711aaaaca127d34fdd
Leïla Pelletier <leila.plltr@gmail.com>
2025-07-23 11:29:59 +0200

    Création du fichier JS pour l'authentification

---
+++ FrontEnd/assets/login.js
@@ -0,0 +1,4 @@
+// Récupération des éléments du dom
+const form = document.querySelector("form");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
