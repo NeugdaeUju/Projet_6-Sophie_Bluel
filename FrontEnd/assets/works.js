4721b20991c10494debd22dd14a5f5c72c342b6e
Leïla Pelletier <leila.plltr@gmail.com>
2025-08-12 11:36:42 +0200

    Récupérer les élements du formulaire pour vérifier qu'ils sont correctement remplis

--- FrontEnd/assets/works.js
+++ FrontEnd/assets/works.js
@@ -372,32 +372,15 @@ bouttonImage.addEventListener("change", () => {
     }
 })
 
+// On récupère les élements du formulaire
+const imageInput = document.querySelector(".modale__addWorks__form__addImage");
+const titleInput = document.querySelector("#modale__addWorks__text");
+const categorieSelect = document.querySelector("#modale__addWorks__category");
+const boutonValider = document.querySelector(".modale__addWorks__submit");
+
 // On vérfie que tous les éléments du formulaire d'ajout soient complété
 formulaireAjout.addEventListener("submit", (e) => {
     // On évite le rechargement de la page
     e.preventDefault();
-    console.log("submit capturé !")
-    // On récupère les éléments du formulaire
-    const imageInput = document.querySelector(".modale__addWorks__newImage");
-    const titleInput = document.querySelector("#modale__addWorks__text");
-    const categorieSelect = document.querySelector("#modale__addWorks__category");
-    // On cherche à confirmer que les éléments sont remplis
-    const imageOK = imageInput;
-    const titleOK = titleInput.value.trim() !== "";
-    const categorieOK = categorieSelect.value !== "";
-    // On fait une boucle pour vérfier que toutes les conditions sont réunis
-    if(imageOK && titleOK && categorieOK) {
-        console.log("Tous les champs sont remplis , on peut valider!")
-        // On change l'apparence du bouton Valider
-        const boutonValider = document.querySelector(".modale__addWorks__submit");
-        boutonValider.classList.remove("modale__addWorks__submit");
-        boutonValider.classList.add("modale__addWorks__submit--OK");
-        console.log("Le formulaire peut être envoyé!");
-    } else {
-        console.log("Il manque un ou plusieurs éléments !");
-        if(!imageOK) alert ("Voys devez choisir une image !");
-        if(!titleOK) alert ("Vous devez renseigner un titre !");
-        if(!categorieOK) alert ("Vous devez choisir une catégorie !");
-    }
-
+    console.log("submit capturé !");
 })
