a1e2562fde04bb5f9c9154ea6e36c601d76dfc7f
Leïla Pelletier <leila.plltr@gmail.com>
2025-08-19 11:43:10 +0200

    Tests des supression et rajout de travaux

--- FrontEnd/assets/works.js
+++ FrontEnd/assets/works.js
@@ -299,7 +299,9 @@ for(let i = 0 ; i < buttonDelte.length ; i++) {
     // Ajout d'une class au bouton pour identifier la position
     buttonDelte[i].classList.add("position_"+i);
     // On écoute le click pour récupérer les infos
-    buttonDelte[i].addEventListener("click", ()=> {
+    buttonDelte[i].addEventListener("click", (event)=> {
+        event.stopPropagation();
+        event.preventDefault();
         // On vérifie sur quelle bouton on a cliquer
         console.log("vous avez clicker sur le bouton "+i);
         // On récupère l'élément parent
@@ -323,9 +325,6 @@ for(let i = 0 ; i < buttonDelte.length ; i++) {
             const works = await reponse.json();
 
             console.log("Status réponse : " , reponseDel.status);
-            generateWorks(works);
-            generateGalleryModale(works);
-
             // Remettre les écouteurs sur les poubelles.
         })
     });
